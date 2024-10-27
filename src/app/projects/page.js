95dcff7c25a0ed1d0434ce9e1a2946af6c962dee
Sambhav Das <130726995+perceptron10100sdas@users.noreply.github.com>
2024-10-27 19:12:12 +0530

    hi 2

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,12 +1,11 @@
+"use client"
 import React from 'react'
-
 import Header from '../Components/header'
 
 export default function page() {
   return (
     <div>
         <Header/>
-
     </div>
   )
 }
